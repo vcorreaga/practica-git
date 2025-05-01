6b5791c62277f18c89acebb6f0cc4cc50f48507d
manjarrezcristian <mariasuarez20209@gmail.com>
2025-04-30 19:33:36 -0500

    actualizar rama

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,11 @@
-import java.util.Scanner
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args){
-        Scanner sc = new Scanner(System.in):
+        Scanner sc = new Scanner(System.in);
+        System.out.println("¡Hola mundo!");
+        System.out.println("¡Hola mundo!");
+        System.out.println("¡Hola mundo!");
+        System.out.println("¡Hola mundo!");
 
     }
 }
